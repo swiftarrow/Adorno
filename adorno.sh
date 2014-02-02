b8c65fea53d6d18cc537eb5ba15a5bbf6e027740
Linkesh Diwan <swiftarrow9@gmail.com>
2014-02-02 11:39:10 +0100

    Completely new version.  Added a configuration file.  Added error Checking.  Touched up a typo in the License.

--- adorno.sh
+++ adorno.sh
@@ -6,30 +6,137 @@
 
 echo 'Welcome to Adorno!'
 
-if ! command -v pythonbrew
-then
-    # pythonbrew was NOT found
-    # We're on Round 1
-    
-    echo 'We will now prepare your system to Tango!'
+# Cleanup and Control
+
+echo 'Changing to Project Directory:'
+cd /vagrant/
+
+temp_file="adorno_second_round_marker"
+
+function Adorno_Unset() {
+    unset ad_project_name ad_current_python ad_required_python ad_install_python ad_install_pythonbrew ad_support_git ad_support_Heroku ad_use_virtualenv ad_virtualenv_name ad_use_pip ad_pip_install_requirements ad_install_packages ad_supplemental_actions ad_supplemental_actions_file ad_success_message
+    if [ -n "$adorno_finito" ]
+        rm $temp_file
+    fi
+    unset adorno_finito
+}
+
+unset adorno_finito
+Adorno_Unset
+
+function Exit_Clean() {
+    adorno_finito="True" #Remove all the files too
+    Adorno_Unset
+    exit 0
+}
+
+function Exit_Unclean() {
+    unset adorno_finito #Do not Remove Files
+    Adorno_Unset
+    exit 0
+}
+
+
+# Search for Adorno Project Setup File
+# If it doesn't exist, fail gracefully
+function Check_Adorno_Setup() {
+    echo 'Searching for Project Setup File...'
+    source $(ls | grep _adorno.sh)
+    if [ -z "$ad_project_name" ]
+    then
+        # -z ad_project_name is empty.  NOTE: -n checks if it is not empty.
+        if  [ -e $temp_file ]
+        then    # This means that we've executed once already, so we must be doing the tango install.
+            curl -s https://raw.github.com/swiftarrow/Adorno/master/tango_adorno.sh | bash
+            echo "Continuing towards a Tango with Django"
+        else
+            echo 'Adorno Project Setup Script is not found or not valid.'
+            while true; do
+                read -p "Shall we prepare for a Tango with Django?" yn
+                case $yn in
+                    [Yy]* ) curl -s https://raw.github.com/swiftarrow/Adorno/master/tango_adorno.sh | bash; break;;
+                    [Nn]* ) echo "You need to download the Adorno Project Setup Script from the project's repository.  It is a shell script file called '*_adorno.sh'. Find it, download it, and put it next to adorno.sh.  Then try again.  Good Luck!"; Exit_Clean;;
+                    * ) echo "Please answer yes or no.";;
+                esac
+            done
+        fi
+    else
+        echo "We will now prepare your system for $ad_project_name"
+    fi
+}
+
+function Check_Python() {
+    # Check if python exists.
+    which python &>/dev/null
+    if [ $? -eq 0 ]
+    then
+    #    echo "Python exists!"
+        ad_current_python=$(python -c 'import sys; print("%s.%s.%s" % sys.version_info[:3])')
+    else
+        ad_current_python="0.0.0"
+    #     echo "No Python Found!"
+    fi
+
+    # If we have the right version of Python, then continue.  Else, note that we need to install python.
+    if [ "$ad_current_python" == "$ad_required_python" ]
+    then
+#         sleep 0
+        unset ad_install_python
+        echo "Python Dependencies Satisfied."
+    else
+        ad_install_python="True"
+        echo "Couldn't find the right version of python, so preparing to install it.  Hang Tight!"
+    fi
+}
+
+# NOTE If we install python at all, we install it via pythonbrew
+
+function Install_Python() {
+    echo "Installing Python $ad_required_python"
+    pythonbrew install $ad_required_python
+
+    echo "Enabling Python $ad_required_python"
+    pythonbrew switch $ad_required_python
+
+    echo 'Sanity Check: is the right version of Python running?'
+    Check_Python
+}
+
+
+function Check_PythonBrew() {
+    which pythonbrew &>/dev/null
+    if [ $? -eq 0 ]
+    then
+        echo "PythonBrew installed successfully!"
+        unset ad_install_pythonbrew
+        rm $temp_file
+    else
+        if grep -q "pythonbrew" ~/.bashrc
+        then
+            # Pythonbrew command doesn't work, but pythonbrew is in the bashrc: Something Sinister
+            echo 'ERROR: Could not find PythonBrew. Something bad happened.'
+            echo "The PythonBrew Installer must have  problems, because Adorno has done it's job properly."
+            echo "You might have to do this yourself.  Sorry about that.  :("
+            echo "I'm leaving the temp file in place so you can debug.'"
+            Exit_Unclean
+        else
+            # Pythonbrew command doesn't work, and pythonbrew is not in the bashrc: We haven't installed it yet.'
+            ad_install_pythonbrew="True"
+        fi
+    fi
+}
+
+
+function Install_PythonBrew() {
+    echo "Preparing to install PythonBrew"
     echo 'Installing Dependencies:'
     
     sudo apt-get -y install build-essential g++ libbz2-dev libdb5.1-dev libexpat1-dev libncurses5-dev libreadline-dev libreadline6-dev libssl-dev libsqlite3-dev libxml2-dev libxslt-dev make zlib1g-dev
-    
-    echo 'Moving to Vagrants Home Folder if Necessary'
-    
-    cd ~
-
-    echo 'Removing Vagrant postinstall.sh'
-
-    rm ~/postinstall.sh
 
     echo 'Downloading and Running the PythonBrew Installer:'
-
     curl -kL http://xrl.us/pythonbrewinstall | bash
 
     echo 'Adding PythonBrew to .bashrc.'
-
     echo '[[ -s $HOME/.pythonbrew/etc/bashrc ]] && source $HOME/.pythonbrew/etc/bashrc' >> ~/.bashrc
 
     echo 'Sanity Check: is PythonBrew in .bashrc?'
@@ -37,63 +144,95 @@ then
     if grep -q "pythonbrew" ~/.bashrc
     then
         echo 'Yes it is! Success! Next steps:'
-        echo '1. Log back with: vagrant ssh'
+        echo '1. If using Vagrant, log back with: vagrant ssh'
         echo '2. Run adorno.sh script again to continue'
-        exit 0
+        touch $temp_file
+        Exit_Unclean
     else
         echo 'Could not find it. Something bad happened.'
         echo 'Sorry, I dont know what to do.'
         echo 'Try looking at the code in adorno.sh and running the commands manually.'
+        Exit_Clean
     fi
+}
 
-else
-    # pythonbrew was found
-    # We're on Round 2
 
-    which pythonbrew
+####################################
+# Here is where the work begins!!! #
+####################################
 
-    echo 'Now we will continue the prepartion for Tango!'
+Check_Adorno_Setup
 
-    echo 'Installing Python 2.7.5:'
+Check_Python
+if [ -n "$ad_install_python" ]
+then
+    # If we need to install Python
+    Check_PythonBrew
+    if [ -n "$ad_install_pythonbrew" ]
+    then
+        #if we need to install pythonbrew
+        Install_PythonBrew
+    else
+        Install_Python
+fi
 
-    pythonbrew install 2.7.5
+echo "Removing Vagrant postinstall.sh"
+rm ~/postinstall.sh
 
-    echo 'Switching to Python 2.7.5'
-
-    pythonbrew switch 2.7.5
-
-    echo 'Sanity Check: which version of Python is running:'
-
-    which python
-
+if [ -n "$ad_use_virtualenv" ]
+then
+    ad_use_pip="True"
     echo 'Installing Dependencies: python-dev python-pip'
-
     sudo apt-get -y install python-dev python-pip
 
     echo 'Downloading and running the VirtualEnv Burrito Installer:'
-
     curl -s https://raw.github.com/brainsik/virtualenv-burrito/master/virtualenv-burrito.sh | bash
 
     echo 'Starting VirtualEnv Burrito:'
-
     source ~/.venvburrito/startup.sh
+    
+    if [ -n "$ad_virtualenv_name" ]
+    then
+        mkvirtualenv $ad_virtualenv_name
+    fi
+fi
 
-    echo 'Changing to the Shared Directory:'
+echo "Changing to Project Directory:"
+cd /vagrant/
 
-    cd /vagrant/
-
+if [ -n "$ad_support_git" ]
+then
     echo 'Downloading .gitignore (needed by Git):'
-
     wget https://raw.github.com/swiftarrow/Adorno/master/Git_Ignore
     mv Git_Ignore .gitignore
-
-    echo 'Downloading Procfile (needed by Heroku):'
-
-    wget https://raw.github.com/swiftarrow/Adorno/master/Procfile
-
-    echo 'Initial setup completed.  Carefully inspect the text above.'
-    echo 'IF THERE ARE NO ERRORS, take a look at the instructions file.'
-    echo 'Armed with your new knowledge, continue with section 2.2.2 of'
-    echo 'www.TangoWithDjango.com/book/chapers/requirements.html'
-
 fi
+
+if [ -n "$ad_support_Heroku" ]
+then
+    echo 'Downloading Procfile (needed by Heroku):'
+    wget https://raw.github.com/swiftarrow/Adorno/master/Procfile
+fi
+
+if [ -n "$ad_pip_install_requirements" ]
+then
+    pip install -r requirements.txt
+fi
+
+if [ -n "$ad_install_packages" ]
+then
+    echo "Installing additional packages..."
+    sudo apt-get install -y $ad_install_packages
+fi
+
+if [ -n "$ad_supplemental_actions" ]
+then
+    echo "Executing Supplemental Actions..."
+    source $ad_supplemental_actions_file
+fi
+
+echo "Initial setup completed.  Carefully inspect the messages above.  If there are no errors, take a look at the Adorno README to learn how to use the stuff we've installed."
+echo " "
+echo $ad_success_message
+
+adorno_finito="True"
+Adorno_Unset
---
+++ tango_adorno.sh
@@ -0,0 +1,140 @@
+# Adorno Project Setup Script.
+# Setup for Tango with Django
+# Author: Linkesh Diwan
+# Licesnse: Peaceful Open Source License (PeaceOSL)
+
+# NOTE
+# Set a variable by setting it equal to a string.  
+# Unset (or falsify) by removing everything after the =
+# "True" is merely a mnemonic.
+# To set as False, COMMENT the line.
+
+# The project name must be set.  Otherwise, the script will fail.
+ad_project_name="Tango With Django"
+
+# What version of Python is required?
+ad_required_python="2.7.5"
+# If Python is NOT required, then uncomment the following line:
+# ad_required_python="0.0.0"
+
+# Do you want to use Git version controls?  This option will download a custom .gitignore file.  If you have your own .gitignore in your repos, then leave this false.
+ad_support_git="True"
+
+# Heroku is a cool way to stage your Django Projects online.
+ad_support_Heroku="True"
+
+# Do you want to use VirtualEnv? (Recommended)
+ad_use_virtualenv="True"
+
+# Do you want to name the virtual environment?
+ad_virtualenv_name="Tango_Django"
+
+# If you have a requirements.txt file, the requirements can be installed by adorno automatically.
+# ad_pip_install_requirements="True"
+
+# Use this to install additional deb packages.  This is executed as: sudo apt-get install -y $ad_install_packages
+# ad_install_packages=""
+
+# Use a supplemental actions file to do other things that Adorno doesn't support.  Custom software installations, etc.  Your script will be executed before Adorno exits.
+# ad_supplemental_actions="True"
+# ad_supplemental_actions_file=""
+
+# A final message to the new developer:
+ad_success_message="Armed with your new knowledge, continue with section 2.2.2 of www.TangoWithDjango.com/book/chapers/requirements.html"
+
+
+# # TESTING
+# 
+# # dm_required_python="2.7.3"
+# # dm_required_python="2.7.5"
+# 
+# 
+# # TESTING functions
+# function test_function() {
+#         echo "horay!!!"
+# }
+# 
+# test_function
+# 
+# testvar=123
+# 
+# unset testvar
+# 
+# if [ -n "$testvar" ]
+# then
+#     echo "true"
+# else
+#     echo "false"
+# fi
+# # # TESTING CAN WE RUN
+# # temp_file="adorno_temp"
+# # thecommand=" --version"
+# # 
+# # python $thecommand
+# # # touch $temp_file
+# # if [ -e $temp_file ]
+# # then
+# #    echo "File $FILE exists."
+# # else
+# #     echo "File does not exist"
+# # fi
+# 
+# # TESTING CHECKING PYTHON
+# 
+# # echo "Check if python exists at all"
+# # 
+# # which python &>/dev/null
+# # 
+# # if [ $? -eq 0 ]
+# # then
+# #     echo "Python exists!"
+# #     dm_current_python=$(python -c 'import sys; print("%s.%s.%s" % sys.version_info[:3])')
+# # else
+# #     unset dm_current_python
+# #     echo "No Python Found!"
+# # fi
+# # 
+# # 
+# # #test
+# # echo "Current Python Version $dm_current_python"
+# # 
+# # # If there is no python installed, then install pythonbrew and 
+# # 
+# # if [ -z "$dm_current_python" ]
+# # then
+# #     dm_install_pythonbrew="True"
+# #     echo "dm_install_pythonbrew = $dm_install_pythonbrew"
+# # else
+# #     dm_current_python=$(python -c 'import sys; print("%s.%s.%s" % sys.version_info[:3])')
+# #     echo "The installed version of Python is $dm_current_python"
+# # fi
+# # 
+# # 
+# # if [ "$dm_current_python" == "$dm_required_python" ]
+# # then
+# #     echo "Do not install Python"
+# # else
+# #     echo "Need to install Python"
+# # fi
+# 
+# # 
+# # current_version=$(python --version | grep "Python ")
+# # # echo ${current_version% *}
+# # echo $current_version
+# # echo ${current_version//"Python "/""}
+# # 
+# # # echo ${stringZ//abc/xyz}      # xyzABC123ABCxyz
+# #                               # Replaces all matches of 'abc' with # 'xyz'.
+# #                               
+# # current=`ls`
+# # # echo $current
+# # 
+# # python --version >> temp
+# # cv_py=`cat temp`
+# # echo $cv_py
+# # 
+# # python -c 'import sys; print(sys.version_info[0])'  sys.version_info[1]  sys.version_info[2])'
+# 
+# function Adorno_Unset() {
+#     unset dm_project_name dm_current_python dm_required_python dm_install_pythonbrew
+# }
